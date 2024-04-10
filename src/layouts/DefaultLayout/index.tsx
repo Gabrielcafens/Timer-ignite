1a17a79d183b55a60337ec450709d3b97da198fd
gabrielNETEAM <gabriel.cafe@ufrpe.br>
2024-04-09 22:20:36 -0300

    fix Páginas e rotas - Componente: Header

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,14 +1,13 @@
-import styled from 'styled-components'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/Header'
 
-export const LayoutContainer = styled.div`
-  max-width: 74rem;
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
-  padding: 2.5rem;
+import { LayoutContainer } from './styles'
 
-  background: ${(props) => props.theme['gray-800']};
-  border-radius: 8px;
-
-  display: flex;
-  flex-direction: column;
-`
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
